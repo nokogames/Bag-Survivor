7c23e7284c07b16692e31e3509bcdd6fb7f8a8ba
Tarik Tanriverdi <39908766+tariktanriverdi@users.noreply.github.com>
2024-07-25 19:33:26 +0300

    MainInventory

--- Assets/Main/_Project/Scripts/SkillManager/SkillVisualBehaviour.cs
+++ Assets/Main/_Project/Scripts/SkillManager/SkillVisualBehaviour.cs
@@ -5,36 +5,41 @@ using System.Collections.Generic;
 using _Project.Scripts.UI.Inventory;
 using _Project.Scripts.UI.Inventory.Behaviours;
 using UnityEngine;
-namespace _Project.Scripts.SkillManagement{
+namespace _Project.Scripts.SkillManagement
+{
 
-public class SkillVisualBehaviour : Dragable
-{   
-    private CustomInventoryData _customInventoryData;
-
-    internal void Initialize(CustomInventoryData customInventoryData, SkillVisualData skillVisualData,InventoryManager inventoryManager)
-    {  
-        _inventoryManager=inventoryManager;
-        _customInventoryData = customInventoryData;
-        onSelectParentTransform = customInventoryData.onSelectParent.GetComponent<RectTransform>();
-        ItemHolder = customInventoryData.itemHolder;
-        _skillVisualData = skillVisualData;
-        size=skillVisualData.Size;
-       // _img.mainTexture=skillVisualData.
-    }
-
-    // Start is called before the first frame update
-    void Start()
+    public class SkillVisualBehaviour : Dragable
     {
+        private CustomInventoryData _customInventoryData;
+
+        internal void Initialize(CustomInventoryData customInventoryData, SkillVisualData skillVisualData, InventoryManager inventoryManager)
+        {
+            _inventoryManager = inventoryManager;
+            _customInventoryData = customInventoryData;
+            onSelectParentTransform = customInventoryData.onSelectParent.GetComponent<RectTransform>();
+            ItemHolder = customInventoryData.itemHolder;
+            _skillVisualData = skillVisualData;
+            size = skillVisualData.Size;
+            if (skillVisualData.img != null)
+            {
+
+                _img.sprite = skillVisualData.img;
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
 
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-
-}
 
 }
